a2d798f39c073d795f9eda70fa9f7772d85dc702
Mandy Liu <manling512@gmail.com>
2023-11-17 18:45:21 -0700

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+new_store.save
+
+puts "\nError messages:"
+new_store.errors.full_messages.each do |message|
+  puts message
+end
